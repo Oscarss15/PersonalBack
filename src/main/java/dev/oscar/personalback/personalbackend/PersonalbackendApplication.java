a15ec5d9e23ddd96afef1b4cf7bc8a0f77e0148d
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-23 19:36:56 +0200

    creacion de proyecto personal back

---
+++ src/main/java/dev/oscar/personalback/personalbackend/PersonalbackendApplication.java
@@ -0,0 +1,13 @@
+package dev.oscar.personalback.personalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/oscar/personalback/personalbackend/PersonalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oscar.personalback.personalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
